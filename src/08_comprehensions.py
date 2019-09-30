e3bd3513743920218ed4540b506457ba8c26e7c5
Liv Johnson <livjab@gmail.com>
2019-09-30 18:35:54 -0500

    finished through #10

--- src/08_comprehensions.py
+++ src/08_comprehensions.py
@@ -2,7 +2,7 @@
 List comprehensions are one cool and unique feature of Python.
 They essentially act as a terse and concise way of initializing
 and populating a list given some expression that specifies how
-the list should be populated. 
+the list should be populated.
 
 Take a look at https://docs.python.org/3/tutorial/datastructures.html#list-comprehensions
 for more info regarding list comprehensions.
@@ -10,32 +10,29 @@ for more info regarding list comprehensions.
 
 # Write a list comprehension to produce the array [1, 2, 3, 4, 5]
 
-y = []
-
+y = [x for x in range(1, 6, 1)]
 print (y)
 
 # Write a list comprehension to produce the cubes of the numbers 0-9:
 # [0, 1, 8, 27, 64, 125, 216, 343, 512, 729]
 
-y = []
 
-print(y)
+squares = [x**3 for x in range(10)]
+print(squares)
 
 # Write a list comprehension to produce the uppercase version of all the
 # elements in array a. Hint: "foo".upper() is "FOO".
 
 a = ["foo", "bar", "baz"]
 
-y = []
-
-print(y)
+upper = [x.upper() for x in a]
+print(upper)
 
 # Use a list comprehension to create a list containing only the _even_ elements
 # the user entered into list x.
 
 x = input("Enter comma-separated numbers: ").split(',')
-
+#x = [1, 2, 3, 4, 5, 6]
 # What do you need between the square brackets to make it work?
-y = []
-
-print(y)
+evens = [i for i in x if (i % 2) == 0]
+print(evens)
